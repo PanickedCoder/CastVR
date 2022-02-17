eca4ae4ac086f4395d27cec9d7a148c98832d78f
PanickedCoder <43437244+PanickedCoder@users.noreply.github.com>
2022-02-17 00:40:01 +0000

    Build V0.01
    Implements the Grimoire, magic switching, and shifts all of the controlling systems to pure VR for showcasing.

--- Intermediate/Build/Win64/CastVR/Shipping/Core/SharedPCH.Core.cpp
+++ Intermediate/Build/Win64/CastVR/Shipping/Core/SharedPCH.Core.cpp
@@ -1,2 +1,2 @@
 // Compiler: 14.26.28806
-#include "C:/Users/mario/Documents/Unreal Projects/CastVR/Intermediate/Build/Win64/CastVR/Shipping/Core/SharedPCH.Core.h"
+#include "C:/Users/mario/Documents/GitHub/CastVR/Intermediate/Build/Win64/CastVR/Shipping/Core/SharedPCH.Core.h"
